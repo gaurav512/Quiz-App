e2d13a59ca4b7b0da32e40ba9a714b9fbdd8b8be
Gaurav <itsgaurav01@gmail.com>
2020-11-11 19:17:44 +0530

    Initial commit

---
+++ end.js
@@ -0,0 +1,28 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = "Your score: " + mostRecentScore;
+
+const MAX_HIGH_SCORES = 5;
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+console.log(highScores);
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+})
+
+saveHighScore = (e) => {
+    e.preventDefault();
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score);
+    highScores.splice(MAX_HIGH_SCORES);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+
+}
