823381539c0fcf29661eef27ee681248796cb42c
Gaurav <itsgaurav01@gmail.com>
2020-11-11 20:02:03 +0530

    minor changes

--- end.js
+++ end.js
@@ -23,6 +23,6 @@ saveHighScore = (e) => {
     highScores.splice(MAX_HIGH_SCORES);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('./');
 
 }
